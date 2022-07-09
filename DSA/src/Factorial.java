84d79f9b46ec14f51e69e349fd17cf188c0ebb6a
Aditya kumar <kumaditya007@gmail.com>
2022-07-09 23:56:38 +0530

    DSA 1.2 factorial

---
+++ DSA/src/Factorial.java
@@ -0,0 +1,32 @@
+
+public class Factorial {
+
+	/*
+	 * static int factorial(int num) { int fact = 1;
+	 * 
+	 * while (num > 0) {
+	 * 
+	 * if (num == 0) return 1; fact = num * fact; num--; }
+	 * 
+	 * return fact;
+	 * 
+	 * }
+	 */
+
+	// Recursive
+
+	static int factorial(int num) {
+
+		if (num == 0)
+			return 1;
+
+		return num * factorial(num - 1);
+
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(factorial(5));
+	}
+
+}
